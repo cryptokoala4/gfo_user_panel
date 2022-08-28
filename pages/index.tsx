15034989b7b673f9c7bdf190001898ddb4283429
Jacky Lee <jackywhlee@gmail.com>
2022-08-29 04:25:55 +0800

    style entry button to panel

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import Button from "../components/Button";
 
 const Home: NextPage = () => {
   return (
@@ -13,7 +14,11 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <Link href="/user-panel">User Panel Access</Link>
+        <Link href="/user-panel">
+          <button className="bg-blue-600 hover:bg-blue-700 focus:ring-blue-800 focus:outline-none text-white font-medium rounded-lg text-sm px-5 py-2.5">
+            User Panel Access
+          </button>
+        </Link>
       </main>
 
       <footer className={styles.footer}>by jackywhlee</footer>
